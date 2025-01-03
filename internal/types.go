bab19cb4e2cbe40f8d6613a2f0997a70683c8840
cleanupDev <philip.goddinger@gmail.com>
2025-01-03 22:44:58 +0100

    rewrite: templ support

---
+++ internal/types.go
@@ -0,0 +1,8 @@
+package internal
+
+type Article struct {
+	ID      int
+	Title   string
+	Summary string
+	Content string
+}
--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"html/template"
-	"net/http"
-	"sort"
-
+	"github.com/cleanupDev/blog/internal"
+	"github.com/cleanupDev/blog/pages"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-type Post struct {
-	ID    int
-	Title string
+var articles = []internal.Article{
+	{ID: 1, Title: "Hello World", Summary: "My first post!", Content: "Full content..."},
+	{ID: 2, Title: "Another Post", Summary: "Check it out!", Content: "More details..."},
 }
 
 func main() {
@@ -22,45 +20,10 @@ func main() {
 
 	e.Static("/static", "static")
 
-	e.GET("/", homeHandler)
-
-	e.GET("/test-htmx", func(c echo.Context) error {
-		return c.String(http.StatusOK, "<p>Dynamic content loaded via HTMX!</p>")
+	e.GET("/", func(c echo.Context) error {
+		component := pages.HomePage(articles)
+		return component.Render(c.Request().Context(), c.Response().Writer)
 	})
 
-	e.GET("/posts/:id", func(c echo.Context) error {
-		id := c.Param("id")
-		return c.String(http.StatusOK, "Post ID: "+id)
-	})
-	e.GET("/posts", postsHandler)
-
-	e.Start(":3000")
-}
-
-func homeHandler(c echo.Context) error {
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
-
-	data := map[string]interface{}{
-		"Title": "simple blog",
-	}
-	return tmpl.Execute(c.Response().Writer, data)
-}
-
-func postsHandler(c echo.Context) error {
-	posts := []Post{
-		{ID: 3, Title: "Post 3"},
-		{ID: 1, Title: "Post 1"},
-		{ID: 2, Title: "Post 2"},
-	}
-
-	sort.Slice(posts, func(i, j int) bool {
-		return posts[i].ID < posts[j].ID
-	})
-
-	tmpl := template.Must(template.ParseFiles("templates/posts.html"))
-
-	data := map[string]interface{}{
-		"Posts": posts,
-	}
-	return tmpl.Execute(c.Response().Writer, data)
+	e.Logger.Fatal(e.Start(":3000"))
 }
